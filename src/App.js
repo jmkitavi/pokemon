c79f9bf781efc84e50a9486d235549e704b83917
jmkitavi <kitavimjoseph@gmail.com>
2020-07-31 22:22:47 +0300

    fetch pokemon cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
-import React from 'react'
-import { SafeAreaView, View, Text, StatusBar } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import {
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  Text
+} from 'react-native'
+import axios from 'axios'
 
 const App = () => {
+  const [pokemons, setPokemons] = useState([])
+
+  const getPokemons = async () => {
+    const res = await axios.get(`https://api.pokemontcg.io/v1/cards`)
+
+    const pokemons = res.data.cards
+
+    setPokemons(pokemons)
+  }
+
+  useEffect(() => {
+    getPokemons()
+  }, [])
+
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <View>
-          <Text> Home </Text>
-        </View>
+        {pokemons.length > 0 &&
+          <FlatList
+          initialNumToRender={3}
+            data={pokemons}
+            renderItem={({ item }) =>
+              <Text>{item.name}</Text>
+            }
+            keyExtractor={item => item.id}
+          />
+        }
+
       </SafeAreaView>
     </>
   )
